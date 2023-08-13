cd0e660b6b7bdcfbd1f86479da95572268369a60
yamunar04 <135696530+yamunar04@users.noreply.github.com>
2023-08-13 10:40:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Complex composition
+
+In <code>src/components/App.js</code>,
+there are 2 components apart from <code>App</code>
+1) <code>Fruits</code>
+2) <code>TypesOfFruits</code>
+
+Your task is to render <code>TypesOfFruits</code> inside <code>Fruits</code>
+and render <code>Fruits</code> inside <code>App</code> in their respective divs without
+changing any div id.
+
---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../styles/App.css';
+const TypesOfFruit = () => {
+  return (
+    <div id="typesOfFruits">
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+};
+
+const Fruits = () => {
+  return (
+    <div id="fruits">
+      { /* Change code below this line */ }
+
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+const App = () => {
+
+  return (
+    <div id="main">
+      <h1>Types of Food:</h1>
+      { /* Change code below this line */ }
+
+      { /* Change code above this line */ }
+    </div>
+  )
+}
+
+
+export default App;
